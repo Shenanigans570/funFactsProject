6ffc5a5776a7e80100895adb2514ffef08770c80
Ryan <sullivan050ryan@gmail.com>
2022-12-05 16:55:30 -0800

    added classes to new p, h1 and stuff

--- index.js
+++ index.js
@@ -3,15 +3,30 @@
 
 const renderFish = (fishObj) => {
     console.log(fishObj)
+    //grabbing parent elements and creating new child elements
     const grabLiveWell = document.getElementById('liveWell')
-    const namePTag = document.createElement('p')
+    const nameH1Tag = document.createElement('h1')
+    const healthBenefitsPTag = document.createElement('p')
+    const proteinPTag = document.createElement('p')
+    const tastePTag = document.createElement('p')
+
     const createImgTag = document.createElement('img')
-    grabLiveWell.append(namePTag, createImgTag)
+//add classes to name, protein, taste, health benefits
+    nameH1Tag.classList.add('fishName')
+    healthBenefitsPTag.classList.add('healthBenefits')
+    proteinPTag.classList.add('Protein')
+    tastePTag.classList.add('Taste')
+    //appending new elements
+    grabLiveWell.append(nameH1Tag, createImgTag, healthBenefitsPTag, proteinPTag, tastePTag)
 
 
     //setting attributes to show the individual fish
     createImgTag.src = fishObj["Species Illustration Photo"].src
-    
+    nameH1Tag.innerText = fishObj['Species Name']
+    healthBenefitsPTag.textContent = fishObj['Health Benefits']
+    proteinPTag.textContent = fishObj['protein']
+    tastePTag.textContent = fishObj['Taste']
+
 }
 
 
